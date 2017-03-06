3f0db268bc435e678cbfc07a86edda068331b709
Siddhant Gupta <siddhant.gupta@siemens.com>
2017-03-06 11:28:45 +0530

    Initial Files

---
+++ src/MyServlet.java
@@ -0,0 +1,52 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+//@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+    {
+        res.setContentType("text/html");// setting the content type
+        PrintWriter pw = res.getWriter();// get the stream to write the data
+        
+        // writing html in the stream
+        pw.println("<html><body>");
+        pw.println("<button type=\"button\">Request Water</button>");
+        pw.println("</body></html>");
+        
+        pw.close();// closing the stream
+    }
+    
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        // TODO Auto-generated method stub
+    }
+    
+}
