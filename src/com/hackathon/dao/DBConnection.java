001f2ca50ac710a85daea6ec8497a66079727f23
vipul patel <patel.vipul@siemens.com>
2017-03-09 11:52:40 +0530

    Changes by Roshni Dhakate

---
+++ src/com/hackathon/dao/DBConnection.java
@@ -0,0 +1,31 @@
+package com.hackathon.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+class DBConnection 
+{
+	public static void main(String args[]) 
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/hackathondb", "root", "");
+			// here 132.186.96.129 : Roshni system ip
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from plantinfo");
+			
+			while (rs.next())
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  "
+						+ rs.getString(3));
+			con.close();
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e);
+		}
+	}
+	
+}
