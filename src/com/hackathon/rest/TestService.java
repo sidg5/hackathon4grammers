a01d7ac13152c7758a25da72dfb938a823a25c06
Siddhant Gupta <siddhant.gupta@siemens.com>
2017-03-09 11:24:50 +0530

    REST AJAX

---
+++ src/com/hackathon/rest/TestService.java
@@ -0,0 +1,15 @@
+package com.hackathon.rest;
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+ 
+@Path("message")
+public class TestService {
+ 
+	@GET
+	public String getMsg() {
+		return "First ajax rest Call Success";
+
+	}
+ 
+}
