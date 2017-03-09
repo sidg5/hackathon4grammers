83a0c1dbb15d7cab0617fed40074c0d477b5963a
dipti <dipti.garg@siemens.com>
2017-03-09 14:26:53 +0530

    Trying to connect to DB

---
+++ src/com/hackathon/dao/ConnectToDB.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Siemens AG 2017 ALL RIGHTS RESERVED.
+ *
+ * R8  
+ * 
+ */
+
+package com.hackathon.dao;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class ConnectToDB
+{
+    
+    public Connection getDBConnection(){
+        
+        Connection connection= null;
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://132.186.96.129:3306/hackathondb", "root", "");
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            // TODO Auto-generated catch block
+            
+        }
+        return connection;
+        
+    }
+}
+
+
+/*
+ * Copyright (c) Siemens AG 2017 ALL RIGHTS RESERVED
+ *
+ * R8
+ */
--- src/com/hackathon/rest/WaterDispensingRequest.java
+++ src/com/hackathon/rest/WaterDispensingRequest.java
@@ -7,18 +7,28 @@
 
 package com.hackathon.rest;
 
+import java.sql.Connection;
+
 import javax.ws.rs.FormParam;  
 import javax.ws.rs.POST;  
 import javax.ws.rs.Path;  
 import javax.ws.rs.core.Response;  
+
+import com.hackathon.dao.ConnectToDB;
 @Path("/waterDispensing")  
 public class WaterDispensingRequest 
 {
     @POST  
     @Path("/request")  
     public Response addUser(  
-        @FormParam("truckID") int truckID)
+        @FormParam("truckID") String truckID)
     {  
+        
+        System.out.println("truckID:"+ truckID);
+        ConnectToDB connectToDB = new ConnectToDB();
+        Connection dbConnection = connectToDB.getDBConnection();
+        
+        System.out.println("dbConnection:"+ dbConnection);
    
         return Response.status(200)  
             .entity(" Product added successfuly!<br> Id: "+truckID)  
