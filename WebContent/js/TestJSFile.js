6161b2c2f63e5086ca4a3a0ace38219989db260a
vipul patel <patel.vipul@siemens.com>
2017-03-16 10:50:19 +0530

    Changes for JSON

--- WebContent/js/TestJSFile.js
+++ WebContent/js/TestJSFile.js
@@ -1,37 +1,40 @@
 /**
  * 
  */$(document).ready(function () {
-	 
+	 var rootURL = "http://localhost:8080/hackathon4grammers/rest/wines/post";
 	 $('#sampleButtom').click(function (event) {
 	 
 		 $.ajax({
-			   url: 'http://localhost:8080/hackathon4grammers/rest/message',
+				type: 'POST',
+				contentType: 'application/json',
+				url: rootURL,
+				dataType: "json",
+				data: formToJSON(),
+				success: function(data, textStatus, jqXHR){
+					alert(data.id);
+				},
+				error: function(jqXHR, textStatus, errorThrown){
+					alert('addWine error: ' + textStatus);
+				}
+			});
+		/* $.ajax({
+			   url: 'http://localhost:8080/PlantWateringSystem/rest/message',
 			   data: {
 			      format: 'json'
 			   },
 			   success: function(data) {
 				   alert("success");
-				   init();
+				   alert(data);
 			   },
 			   type: 'GET'
-			});
+			});*/
 	
 	 });
-	  var truckID =$('#truckid').val();
-	  //alert("truckID:"+truckID);
-	 $('#tankFill').click(function (event) {
-		
-		 $.ajax({
-			   url: 'http://localhost:8080/hackathon4grammers/rest/waterDispensing/request',
-			   data: {
-			      format: 'json',
-			      'truckid': truckID
-			   },
-			   success: function(data) {
-				   //alert("success");
-			   },
-			   type: 'POST'
-			});
-	
-	 });
+	 
+	 function formToJSON() {
+		return JSON.stringify({
+				"id":"1", 
+				"name":"3"
+				});
+		}
 });
